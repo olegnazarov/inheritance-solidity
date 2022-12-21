e7ab5eab8c94dd100f386267f3dd3dc4de05b462
olegnazarov <5623978+olegnazarov@users.noreply.github.com>
2022-12-21 15:45:58 +0300

    Create addGrandChild function, constructor and onlyOwner modifier

--- contracts/inheritance.sol
+++ contracts/inheritance.sol
@@ -19,4 +19,22 @@ contract GrandDadTreasury {
 
     mapping(address => Grandchild) public grandChilds;
 
+    constructor {
+        owner = msg.sender;
+        grandChildCount = 0;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    function addGrandChild(address wallet, string grandChildName, uint256 birthDate) public onlyOwner {
+        require(birthDate > 0, "Wrong DOB");
+        require(grandChilds[wallet].exist == false, "Wallet already exists");
+        grandChilds[wallet] = Grandchild(grandChildName, birthDate, false, true);
+        grandChildsArray.push(wallet);
+        grandChildCount++;
+    }
+
 }
